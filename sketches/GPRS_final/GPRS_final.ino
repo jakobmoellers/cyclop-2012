a572c6b1b2e18cf7f427ab33dd0397339cf44137
Jakob Moellers <jakob.moellers@gmail.com>
2012-12-19 18:51:00 +0100

    initial commit. some parts are working

---
+++ sketches/GPRS_final/GPRS_final.ino
@@ -0,0 +1,179 @@
+#include <SoftwareSerial.h>
+ 
+SoftwareSerial mySerial(7, 8);
+
+String json = "{\"device_id\": 123456,\"measurements\": [{\"timestamp\": 1355230205,\"cellid\": 109128739432,\"mcc\": 123456,\"mnc\": 123456,\"temperature\": 29,\"humidity\": 80,\"light\": 1,\"acceleration\": {\"x\": 0,\"y\": 0,\"z\": 0},\"compass\": {\"x\": 0,\"y\": 0,\"z\": 0},\"battery\": 78}]}";
+
+void setup()
+{
+  mySerial.begin(19200);               // the GPRS baud rate   
+  Serial.begin(19200);    // the GPRS baud rate 
+  delay(500);
+}
+
+void loop()
+{  if (Serial.available())
+    switch(Serial.read())
+   {
+     case 'p':
+       PowerOnOff();
+       break;
+     case 'g':
+       GSMStatus();
+       break;
+     case 'c':
+       ConnectAndPost();
+       break;     
+     case 's':
+       Serial.print("data=");
+       Serial.println(json);
+       Serial.println("Print Test: " + String("12341"));
+       break;
+     case 'l':
+       Serial.println(String(sizeof(json)));
+       Serial.println("Stuff\x1A");
+       break;       
+   } 
+  if (mySerial.available())
+    Serial.write(mySerial.read());
+  //delay(100);
+}
+
+//Turn on or off 
+void PowerOnOff()
+{
+  pinMode(9, OUTPUT); 
+  digitalWrite(9,LOW);
+  delay(1000);
+  digitalWrite(9,HIGH);
+  delay(2000);
+  digitalWrite(9,LOW);
+  delay(3000);
+}
+
+//Query GSM network registration status
+void GSMStatus(){
+
+  mySerial.println("AT+CPIN?");
+  delay(100); 
+  ShowSerialData();
+  
+  mySerial.println("AT+CSQ");
+  delay(1000);
+  ShowSerialData();
+ 
+  mySerial.println("CREG=2");
+  delay(4000);
+  ShowSerialData();
+
+//  mySerial.println("CREG?");
+//  delay(4000);
+//  ShowSerialData();
+
+}
+
+//----------Start Task and Activate Wireless Connection--------------
+void ConnectAndPost(){
+  mySerial.println("AT+CGATT?");
+  delay(1000);
+  ShowSerialData();
+
+  mySerial.println("AT+CSTT=\"data.access.de\"");//setting the SAPBR, the connection type is using gprs
+  delay(1000);
+  ShowSerialData();
+
+  mySerial.println("AT+CIICR");
+  //bring up wireless connection to GPRS (or CSD)
+  delay(3000);
+  ShowSerialData(); 
+ 
+  mySerial.println("AT+CIFSR");
+  //get local IP adress (as response)
+  delay(1000);
+  ShowSerialData();
+
+  mySerial.println("AT+CIPSTATUS");
+  //Query current connection status
+  delay(1000);
+  ShowSerialData();
+
+  mySerial.println("AT+CIPHEAD=1");
+  //to add an 'IP Header' to receive data
+  delay(1000);
+  ShowSerialData();
+
+//  mySerial.println("AT+CIPCSGP=1");
+//  //query the IP address of the given domain name
+//  delay(1000);
+//  ShowSerialData();
+
+  //mySerial.println("AT+CIPSTART=\"TCP\",\"http://potwech.uni-muenster.de/rest/index.php/post/\",\"80\"");
+  mySerial.println("AT+CIPSTART=\"TCP\",\"128.176.146.214\",\"80\"");//start up the connection
+  //start up connection
+  delay(4000);
+  ShowSerialData();
+ 
+  mySerial.println("AT+CIPSEND");
+  //send data to remote server, CTRL+Z (0x1a) to send 
+  //  >hello TCP server.
+  //  SEND OK
+  //  hello sim900
+  delay(4000);
+  ShowSerialData();
+ 
+   mySerial.println("POST /rest/index.php/post2 HTTP/1.1 ");
+   delay(100);
+   ShowSerialData();
+
+  //PRINT DATA TO mySerial
+   mySerial.println("Host: potwech.uni-muenster.de");
+   delay(100);
+   ShowSerialData();
+   
+   mySerial.println("Accept: application/json");
+   delay(100);
+   ShowSerialData();
+
+   mySerial.println("Content-Length: 23");
+   delay(100);
+   ShowSerialData();   
+   
+   mySerial.println("Content-Type: application/x-www-form-urlencoded");
+   delay(100);
+   ShowSerialData();
+   
+   mySerial.println();
+   delay(100);
+   
+   mySerial.print("data={\"temperature\":66}");
+   delay(100);
+   ShowSerialData();
+   
+  mySerial.println((char)26);//sending
+  delay(1000);//waitting for reply, important! the time is base on the condition of internet 
+  mySerial.println();
+  ShowSerialData(); 
+ 
+  //PRINT DATA TO mySerial
+  mySerial.println((char)26);//sending
+  delay(5000);//waitting for reply, important! the time is base on the condition of internet 
+  mySerial.println();
+  ShowSerialData();
+
+  mySerial.println("AT+CIPCLOSE");//close the TCP connection
+  delay(100);
+  ShowSerialData();  
+
+  mySerial.println("AT+CIPSHUT");//close the GPRS connection
+  delay(100);
+  ShowSerialData(); 
+}
+
+void ShowSerialData()
+{
+  while(mySerial.available()!=0)
+    Serial.write(mySerial.read());
+    delay(500);
+    Serial.println();
+    Serial.println("------");
+}
---
+++ sketches/rtc_dht_acc_light_sd/rtc_dht_acc_light_sd.ino
@@ -0,0 +1,135 @@
+// Prototype of POW
+// TODO: correct temp values; float to string
+//---------Libs to Include-----------
+#include <SD.h>
+#include <Wire.h> //I2C
+#include <RTClib.h> 
+#include <DHT.h>
+#include <LSM303.h> //Accelerometer
+//-------Compass/Accelerometer-------
+LSM303 accelerometer;
+int accArray[3];
+int compArray[3];
+//-------Photodiode--------
+int lightPin = A3;
+int lightValue;
+//--------RTC Objects-----------
+RTC_DS1307 RTC;
+DateTime time;
+//--------SD Values------------
+const int chipSelect = 10;
+File dataFile;
+//---------DHT Variables and Object----------
+#define DHTPIN 2
+#define DHTTYPE DHT22   // DHT 22 (AM2302)
+DHT dht(DHTPIN, DHTTYPE);
+float temperature;
+float humidity;
+//--------String for Json----------
+String json1 = "";
+String json2 = "";
+String acc = "";
+String comp = "";
+//--------Variables for avg Fuctionality---------
+float tempSum = 0;
+float humiSum = 0;
+//-------------Timer Variables------
+int samplingInterval = 2;
+int loopCounter = 0;
+long timeNew;
+long timeOld = 0;
+//-------------Device and Network Values----------------
+int deviceid = 123;
+int cid = 000;
+int mcc = 456;
+int mnc = 789;
+//-------Setup the board and start sensors-------------
+void setup () {
+  Serial.begin(19200);
+  Wire.begin();
+  RTC.begin();
+  dht.begin();
+  Serial.println("Booting the system...");
+  Serial.println("Initializing RTC...");  
+  if (! RTC.isrunning()) {
+    Serial.println("RTC is NOT running!");
+    //set the RTC to the date & time this sketch was compiled
+    RTC.adjust(DateTime(__DATE__, __TIME__));
+  }
+  time = RTC.now();
+  Serial.print("Initializing SD card...");
+  pinMode(10, OUTPUT);
+  // see if the card is present and can be initialized:
+  if (!SD.begin(chipSelect)) {
+    Serial.println("Card failed, or not present");
+    // don't do anything more
+    return;
+  }
+  Serial.println("card initialized.");
+}
+
+void loop () {
+    time = RTC.now();
+    timeNew = time.unixtime();
+    if ((timeNew - timeOld) >= samplingInterval){ //sampling every 2 seconds
+      tempSum = tempSum + dht.readTemperature();
+      humiSum = humiSum + dht.readHumidity();
+      
+      //TODO: Catch Events of Accelerometer and Photodiode
+      Serial.print("The light value is: ");
+      Serial.println(analogRead(3));
+      
+      accelerometer.read();
+      
+      Serial.print("Acceleration is: ");
+      Serial.print("X: ");
+      Serial.print((int)accelerometer.a.x);
+      Serial.print(" Y: ");
+      Serial.print((int)accelerometer.a.y);
+      Serial.print(" Z: ");
+      Serial.println((int)accelerometer.a.z);
+      
+      Serial.print("Compass is: ");
+      Serial.print("X: ");
+      Serial.print((int)accelerometer.m.x);
+      Serial.print(" Y: ");
+      Serial.print((int)accelerometer.m.y);
+      Serial.print(" Z: ");
+      Serial.println((int)accelerometer.m.z);      
+      
+      if (loopCounter == 4){ //Averaging after 10 seconds
+        Serial.print("Calculating average values at ");
+        Serial.println(millis());
+        tempSum = tempSum / 5;
+        humiSum = humiSum / 5;
+        
+        //TODO: Sting buffer problem causes crash of program!! Seperation of strings, change format to CSV, or try out Arduino Mega
+        String json = "{\"device_id\": 123456,\"measurements\": [{\"timestamp\": 1355230205,\"cellid\": 109128739432,\"mcc\": 123456,\"mnc\": 123456}]}";
+        //json1 = "{\"device_id\":" + String(deviceid) + ',' + "\"measurements\":[{\"timestamp\":" + String(timeNew) + ',' + "\"cellid\":" + String(cid) + ',' + "\"mcc\":" + String(mcc) + ',';
+        //json2 = + "\"mnc\":" + String(mnc) + ',' + "\"temperature\":" + String((int)tempSum) + ',' + "\"humidity\":" + String((int)humiSum) + "}]}";
+        
+        //Serial.println(json1);
+        //Serial.println(json2);
+        
+        dataFile = SD.open("datalog.txt", FILE_WRITE);
+
+        // if the file is available, write to it:
+        if (dataFile){
+          dataFile.println(json);
+          dataFile.close();
+          // print to the serial port too:
+          Serial.print(json);
+          Serial.println(" saved to SD!");
+        }  
+        // if the file isn't open, pop up an error:
+        else{
+          Serial.println("Error opening datalog.txt");
+        } 
+        loopCounter = -1;
+        tempSum = 0;
+        humiSum = 0;
+      }
+      loopCounter++;
+      timeOld = timeNew;  
+    }
+}
