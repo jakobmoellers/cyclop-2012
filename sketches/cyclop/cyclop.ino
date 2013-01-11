50c637ae8df3f5956d8f3fc76629091757bb5eef
Jakob Moellers <jakob.moellers@gmail.com>
2013-01-11 13:42:33 +0100

    included dht, heavy refactoring

--- sketches/cyclop/cyclop.ino
+++ sketches/cyclop/cyclop.ino
@@ -5,6 +5,7 @@ This is the main cyclop application
 //Libraries
 #include <string.h>
 #include <ctype.h>
+#include "DHT.h"
 
 //PINs
 const int WaterPin=9; //Water sensor
@@ -14,7 +15,8 @@ int zPin = 4;
 int selPin = 6;
 int sleepPin = 5;
 int rxPin = 0; //GPS
-int txPin = 1;                
+int txPin = 1;  
+#define DHTPIN 10 //DHT
 
 //Variables
 boolean alarm=false; //True if alarm is armed
@@ -31,7 +33,9 @@ char comandoGPR[7] = "$GPRMC";
 int cont=0;
 int bien=0;
 int conta=0;
-int indices[13];
+int indices[13]; 
+#define DHTTYPE DHT11 // DHT 11
+DHT dht(DHTPIN, DHTTYPE);
 
 //Measurements
 boolean rain=false;
@@ -40,14 +44,22 @@ double co_ppm;
 double gValue;
 double lat;
 double lon;
+float temperature;
+float humidity;
+
+/*
+Main methods
+ */
 
 void setup(){
   //Water sensor
   pinMode(WaterPin, INPUT);
+
   //Acc
   digitalWrite(sleepPin,HIGH);
   digitalWrite(selPin,HIGH); 
   getCalValues();
+
   //GPS
   pinMode(rxPin, INPUT);
   pinMode(txPin, OUTPUT);
@@ -55,6 +67,9 @@ void setup(){
   {       // Initialize a buffer for received data
     linea[i]=' ';
   }
+
+  //DHT
+  dht.begin();
 
   Serial.begin(9600);
 
@@ -68,7 +83,84 @@ void loop(){
   //Download new hazards in a time interval of 1 minute
   //What about the measurement process? Also every minute? Should be included here
 
-  //Get current GPS coordinates
+    //Get current GPS coordinates
+  getPosition();
+
+  //Determine whether in alarm mode or standard mode
+  if(alarm==true){
+    //Alarm mode
+
+  } 
+  else {
+    //Standard mode
+
+      //Take measurements
+
+    //Watersensor  
+    int waterSensorValue = digitalRead(WaterPin);
+    if(waterSensorValue==0)
+      rain=true;
+
+    //NO2
+    getNO2();
+
+    //CO
+    getCO();
+
+    //Acc
+    getAcc();
+
+    //DHT
+    humidity = dht.readHumidity();
+    temperature = dht.readTemperature();
+
+    //Store measurements on SD Card
+
+    //Upload measurements
+
+    //Clean SD-Card
+
+  }
+
+
+}
+
+/*
+Help Methods
+ */
+
+//Accelerometer
+void getAcc(){
+  float xValue = analogRead(xPin);
+  float yValue = analogRead(yPin);
+  float zValue = analogRead(zPin);
+  float gxValue = constrain((xValue/scalePoints-1.65-calValues[0])/0.8,-1,1);
+  float gyValue = constrain((yValue/scalePoints-1.65-calValues[1])/0.8,-1,1);
+  float gzValue = constrain((zValue/scalePoints-1.65-calValues[2])/0.8,-1,1);
+  gValue = sqrt(gxValue*gxValue+gyValue*gyValue+gzValue*gzValue);
+}
+
+void getCalValues() {
+  while (numReadings < maxNumReadings)
+    if (millis() - lastTime > dt) {
+      calValues[0] = calValues[0] + analogRead(xPin);
+      calValues[1] = calValues[1] + analogRead(yPin);
+      calValues[2] = calValues[2] + analogRead(zPin);
+      lastTime = millis();
+      numReadings++;
+    }
+  calValues[0] = (calValues[0] / maxNumReadings)/scalePoints-1.65;
+  calValues[1] = (calValues[1] / maxNumReadings)/scalePoints-1.65;
+  calValues[2] = (calValues[2] / maxNumReadings)/scalePoints-0.85;
+}
+
+//GPS
+String return_gps_pos(){
+  String gpspos = String(linea);
+  return gpspos;
+}
+
+void getPosition(){
   byteGPS=Serial.read();         // Read a byte of the serial port
   if (byteGPS == -1) 
   {           // See if the port is empty yet
@@ -106,8 +198,8 @@ void loop(){
           }
         }
         /*Serial.println("");      // ... and write to the serial port
-        Serial.println("");
-        Serial.println("---------------");*/
+         Serial.println("");
+         Serial.println("---------------");*/
         for (int i=0;i<12;i++)
         {
           switch(i)
@@ -157,8 +249,8 @@ void loop(){
             break;
           }
           /*for (int j=indices[i];j<(indices[i+1]-1);j++){
-            Serial.print(linea[j+1]); 
-          }*/
+           Serial.print(linea[j+1]); 
+           }*/
           Serial.println("");
         }
         //Serial.println("---------------");
@@ -170,86 +262,41 @@ void loop(){
       }                 
     }
   }
-
-  //Determine whether in alarm mode or standard mode
-  if(alarm==true){
-    //Alarm mode
-
-  } 
-  else {
-    //Standard mode
-
-      //Take measurements
-
-    //Watersensor  
-    int waterSensorValue = digitalRead(WaterPin);
-    if(waterSensorValue==0)
-      rain=true;
-
-    //NO2
-    //Read values and calculate rs/ro
-    double reading0 = analogRead(4);
-    double sensorValueNo= map(reading0,0,1024,0,500);
-    sensorValueNo= sensorValueNo/100;
-    double rs_no=(22000/((5-sensorValueNo)*sensorValueNo));
-    double rs_r0_no = rs_no / 1000;
-    //Serial.print(" NO2 Rs/R0: ");
-    //Serial.print(rs_r0_no);
-    //Estimate ppm
-    no2_ppm=0.013043*rs_r0_no+0.0086956;
-
-    //CO
-    //Read values and calculate rs/ro
-    double reading1 = analogRead(5);
-    double sensorValueCo=map(reading1,0,1024,0,500);
-    sensorValueCo=sensorValueCo/100;
-    double rs_co=(100000/((5-sensorValueCo)))*sensorValueCo;
-    double rs_r0_co = rs_co / 100000; 
-    //Serial.print(" CO Rs/R0: ");
-    //Serial.print(rs_r0_co);
-    //Estimate ppm
-    co_ppm=-25*rs_r0_co+20.6;
-
-    //Acc
-    float xValue = analogRead(xPin);
-    float yValue = analogRead(yPin);
-    float zValue = analogRead(zPin);
-    float gxValue = constrain((xValue/scalePoints-1.65-calValues[0])/0.8,-1,1);
-    float gyValue = constrain((yValue/scalePoints-1.65-calValues[1])/0.8,-1,1);
-    float gzValue = constrain((zValue/scalePoints-1.65-calValues[2])/0.8,-1,1);
-    gValue = sqrt(gxValue*gxValue+gyValue*gyValue+gzValue*gzValue);
-
-    //Store measurements on SD Card
-
-    //Upload measurements
-
-    //Clean SD-Card
-
-  }
-
-
 }
 
-//Accelerometer calibration
-void getCalValues() {
-  while (numReadings < maxNumReadings)
-    if (millis() - lastTime > dt) {
-      calValues[0] = calValues[0] + analogRead(xPin);
-      calValues[1] = calValues[1] + analogRead(yPin);
-      calValues[2] = calValues[2] + analogRead(zPin);
-      lastTime = millis();
-      numReadings++;
-    }
-  calValues[0] = (calValues[0] / maxNumReadings)/scalePoints-1.65;
-  calValues[1] = (calValues[1] / maxNumReadings)/scalePoints-1.65;
-  calValues[2] = (calValues[2] / maxNumReadings)/scalePoints-0.85;
+//NO2
+
+void getNO2(){
+  //Read values and calculate rs/ro
+  double reading0 = analogRead(4);
+  double sensorValueNo= map(reading0,0,1024,0,500);
+  sensorValueNo= sensorValueNo/100;
+  double rs_no=(22000/((5-sensorValueNo)*sensorValueNo));
+  double rs_r0_no = rs_no / 1000;
+  //Serial.print(" NO2 Rs/R0: ");
+  //Serial.print(rs_r0_no);
+  //Estimate ppm
+  no2_ppm=0.013043*rs_r0_no+0.0086956;
 }
 
-//GPS
-String return_gps_pos(){
-  String gpspos = String(linea);
-  return gpspos;
+//CO
+void getCO(){
+  //Read values and calculate rs/ro
+  double reading1 = analogRead(5);
+  double sensorValueCo=map(reading1,0,1024,0,500);
+  sensorValueCo=sensorValueCo/100;
+  double rs_co=(100000/((5-sensorValueCo)))*sensorValueCo;
+  double rs_r0_co = rs_co / 100000; 
+  //Serial.print(" CO Rs/R0: ");
+  //Serial.print(rs_r0_co);
+  //Estimate ppm
+  co_ppm=-25*rs_r0_co+20.6;
 }
+
+
+
+
+
 
 
 
