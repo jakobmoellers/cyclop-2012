72edb41c124cd54638b819258c2fffd785480f2d
Jakob Moellers <jakob.moellers@gmail.com>
2013-01-15 13:40:48 +0100

    cyclop display change

--- sketches/cyclop/cyclop.ino
+++ sketches/cyclop/cyclop.ino
@@ -105,10 +105,8 @@ void setup(){
   PORTB |= 0x21;
   SeeedOled.clearDisplay();          //clear the screen and set start position to top left corner
   SeeedOled.setNormalDisplay();      //Set display to normal mode (i.e non-inverse mode)
-  SeeedOled.setPageMode();           //Set addressing mode to Page Mode
-  SeeedOled.setTextXY(0,0);          //Set the cursor to Xth Page, Yth Column  
-  SeeedOled.putString("Hello World!"); //Print the String
-
+  SeeedOled.setHorizontalMode();  
+  SeeedOled.putString("Welcome to Cyclop!"); //Print the String
 
 }
 
