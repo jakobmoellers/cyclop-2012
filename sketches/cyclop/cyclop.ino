b30162eef8f8805b20de0cb82ac535193ca2d984
Jakob Moellers <jakob.moellers@gmail.com>
2013-01-17 15:41:49 +0100

    comments

--- sketches/cyclop/cyclop.ino
+++ sketches/cyclop/cyclop.ino
@@ -285,8 +285,6 @@ void takeMeasurements(){
 
 void determineAlertMode(){
 
-  //TODO Build working switch
-
   int magnetSensor = digitalRead(alarmPin);
   if (magnetSensor!=oldTest){
     Serial.println("Alarm mode changed");
@@ -304,16 +302,6 @@ void determineAlertMode(){
 
   }
   delay(200);
-
-  /*int alarmInput = digitalRead(alarmPin);
-   if (alarmInput==0){
-   alarm=true;
-   Serial.println("Alarm activated");
-   } 
-   else {
-   alarm=false;
-   Serial.println("Alarm deactivated");
-   }*/
 
 }
 
@@ -460,8 +448,6 @@ void getNO2(){
   sensorValueNo= sensorValueNo/100;
   double rs_no=(22000/((5-sensorValueNo)*sensorValueNo));
   double rs_r0_no = rs_no / 1000;
-  //Serial.print(" NO2 Rs/R0: ");
-  //Serial.print(rs_r0_no);
   //Estimate ppm
   no2_ppm=0.013043*rs_r0_no+0.0086956;
 }
@@ -474,8 +460,6 @@ void getCO(){
   sensorValueCo=sensorValueCo/100;
   double rs_co=(100000/((5-sensorValueCo)))*sensorValueCo;
   double rs_r0_co = rs_co / 100000; 
-  //Serial.print(" CO Rs/R0: ");
-  //Serial.print(rs_r0_co);
   //Estimate ppm
   co_ppm=-25*rs_r0_co+20.6;
 }
